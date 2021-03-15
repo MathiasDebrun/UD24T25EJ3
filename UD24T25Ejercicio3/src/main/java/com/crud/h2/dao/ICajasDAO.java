ba34f4ffe74a56691bce5647402c9d87818d1d27
MathiasDebrun <mathiasdebrun@hotmail.com>
2021-03-15 20:09:06 +0100

    Ejercicio Completo.

---
+++ UD24T25Ejercicio3/src/main/java/com/crud/h2/dao/ICajasDAO.java
@@ -0,0 +1,12 @@
+package com.crud.h2.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.crud.h2.dto.Cajas;
+/**
+ * @author Estefania
+ *
+ */
+public interface ICajasDAO extends JpaRepository<Cajas, String> {
+
+}
